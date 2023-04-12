875d499d7b59af8fe906224b49b45ed7609694f5
Leonardo do Nascimento Peixoto da Silva <Leonardonps@academicos.com>
2023-04-11 21:44:18 -0300

    Adicionando alunoController

--- projeto/api/src/main/java/com/alunoonline/api/model/Aluno.java
+++ projeto/api/src/main/java/com/alunoonline/api/model/Aluno.java
@@ -8,11 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Aluno {
+public class Aluno implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
