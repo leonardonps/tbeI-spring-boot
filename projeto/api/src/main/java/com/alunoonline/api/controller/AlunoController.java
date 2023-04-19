d25cce52e5c7c5f177072660c2cc40a4faf0c9e7
Leonardo do Nascimento Peixoto da Silva <Leonardonps@academicos.com>
2023-04-18 21:24:45 -0300

    Adicionando mais funcionalidades

--- projeto/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
+++ projeto/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
@@ -2,10 +2,14 @@ package com.alunoonline.api.controller;
 
 import com.alunoonline.api.model.Aluno;
 import com.alunoonline.api.service.AlunoService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aluno")
@@ -20,5 +24,28 @@ public class AlunoController {
         Aluno alunoCreated = service.create(aluno);
         return ResponseEntity.status(201).body(alunoCreated);
     }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id){
+        service.deleteById(id);
+    }
+
+    
+
+
+
 
 }
--- projeto/api/src/main/java/com/alunoonline/api/service/AlunoService.java
+++ projeto/api/src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -2,8 +2,13 @@ package com.alunoonline.api.service;
 
 import com.alunoonline.api.model.Aluno;
 import com.alunoonline.api.repository.AlunoRepository;
+import jakarta.persistence.StoredProcedureParameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -13,5 +18,21 @@ public class AlunoService {
 
     public Aluno create(Aluno aluno){
         return repository.save(aluno);
+    }
+
+    public List<Aluno> findAll(){
+        return repository.findAll();
+    }
+
+    public Optional<Aluno> findById(Long id){
+        return repository.findById(id);
+    }
+
+    public void deleteById(Long id){
+        repository.deleteById(id);
+    }
+
+    public void updateById(Long id){
+
     }
 }
