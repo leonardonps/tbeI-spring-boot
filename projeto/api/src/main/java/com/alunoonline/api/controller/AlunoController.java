38f170b7a538dff5dd904de980f49d912f3a6021
Leonardo do Nascimento Peixoto da Silva <Leonardonps@academicos.com>
2023-04-25 21:24:11 -0300

    Adicionando Professor e Disciplina

--- projeto/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
+++ projeto/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
@@ -43,7 +43,7 @@ public class AlunoController {
         service.deleteById(id);
     }
 
-    
+
 
 
 
---
+++ projeto/api/src/main/java/com/alunoonline/api/model/Disciplina.java
@@ -0,0 +1,25 @@
+package com.alunoonline.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Disciplina implements Serializable {
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn()
+    private Professor professor;
+
+}
