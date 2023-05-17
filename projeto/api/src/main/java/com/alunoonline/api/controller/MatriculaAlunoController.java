b14952ac3985a303ad9b79d0cc91bbccce18dee1
Leonardo do Nascimento Peixoto da Silva <Leonardonps@academicos.com>
2023-05-16 21:02:16 -0300

    Adicionando upgradeGrades no MatriculaAluno

--- projeto/api/src/main/java/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ projeto/api/src/main/java/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -1,6 +1,7 @@
 package com.alunoonline.api.controller;
 
 import com.alunoonline.api.model.MatriculaAluno;
+import com.alunoonline.api.model.dtos.MatriculaAlunoNotasOnlyDto;
 import com.alunoonline.api.service.MatriculaAlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,12 @@ public class MatriculaAlunoController {
         MatriculaAluno matriculaAlunoCreated = service.create(matriculaAluno);
 
         return ResponseEntity.status(201).body(matriculaAlunoCreated);
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void patch(@RequestBody MatriculaAlunoNotasOnlyDto notasOnlyDto,
+                      @PathVariable Long id){
+        service.updateGrades(notasOnlyDto, id);
     }
 }
--- projeto/api/src/main/java/com/alunoonline/api/service/MatriculaAlunoService.java
+++ projeto/api/src/main/java/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -1,12 +1,17 @@
 package com.alunoonline.api.service;
 
 import com.alunoonline.api.model.MatriculaAluno;
+import com.alunoonline.api.model.dtos.MatriculaAlunoNotasOnlyDto;
 import com.alunoonline.api.repository.MatriculaAlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MatriculaAlunoService {
+
+    static final Double GRADEAVGTOAPPROVE = 7.0;
 
     @Autowired
     MatriculaAlunoRepository repository;
@@ -15,8 +20,38 @@ public class MatriculaAlunoService {
         return repository.save(matriculaAluno);
     }
 
-    public void updateGrades(){
+    public void updateGrades(MatriculaAlunoNotasOnlyDto matriculaAlunoNotasOnlyDto, Long matriculaId) {
+        Optional<MatriculaAluno> matriculaAlunoToUpdate = repository.findById(matriculaId);
 
+        boolean needUpdate = false;
+
+
+        if (matriculaAlunoNotasOnlyDto.getNota1() != null) {
+            matriculaAlunoToUpdate.
+                    ifPresent(matriculaAluno -> matriculaAluno.setNota1(matriculaAlunoNotasOnlyDto.getNota1()));
+            needUpdate = true;
+
+        }
+
+        if (matriculaAlunoNotasOnlyDto.getNota2() != null) {
+            matriculaAlunoToUpdate.
+                    ifPresent(matriculaAluno -> matriculaAluno.setNota2(matriculaAlunoNotasOnlyDto.getNota2()));
+            needUpdate = true;
+        }
+
+        if(needUpdate){
+            if(matriculaAlunoToUpdate.get().getNota1() != null
+                    && matriculaAlunoToUpdate.get().getNota2() != null){
+                if((matriculaAlunoToUpdate.get().getNota1() + matriculaAlunoToUpdate.get().getNota2())/2
+                        >= GRADEAVGTOAPPROVE){
+                    matriculaAlunoToUpdate.ifPresent(matriculaAluno -> matriculaAluno.setStatus("APROVADO"));
+                }else{
+                    matriculaAlunoToUpdate.ifPresent(matriculaAluno -> matriculaAluno.setStatus("REPROVADO"));
+                }
+            }
+
+            repository.save(matriculaAlunoToUpdate.get());
+        }
     }
 
 }
