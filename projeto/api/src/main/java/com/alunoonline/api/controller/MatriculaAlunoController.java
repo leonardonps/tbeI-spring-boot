439ad7d1f4e7ca949c79cbeebed005186f0a5b4a
Leonardo do Nascimento Peixoto da Silva <Leonardonps@academicos.com>
2023-05-23 21:24:17 -0300

    Adicionando funções finais do projeto

--- projeto/api/src/main/java/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ projeto/api/src/main/java/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -1,6 +1,7 @@
 package com.alunoonline.api.controller;
 
 import com.alunoonline.api.model.MatriculaAluno;
+import com.alunoonline.api.model.dtos.HistoricoAlunoDto;
 import com.alunoonline.api.model.dtos.MatriculaAlunoNotasOnlyDto;
 import com.alunoonline.api.service.MatriculaAlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +24,22 @@ public class MatriculaAlunoController {
         return ResponseEntity.status(201).body(matriculaAlunoCreated);
     }
 
-    @PatchMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void patch(@RequestBody MatriculaAlunoNotasOnlyDto notasOnlyDto,
+    @PatchMapping("/update-grades/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateGrades(@RequestBody MatriculaAlunoNotasOnlyDto notasOnlyDto,
                       @PathVariable Long id){
         service.updateGrades(notasOnlyDto, id);
+    }
+
+    @PatchMapping("/atualiza-status/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateStatusToLocked(@PathVariable Long id){
+        service.updateStatusToLocked(id);
+    }
+
+    @GetMapping("/historico-aluno/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public HistoricoAlunoDto getHistoricoFromAluno(@PathVariable Long id){
+        return service.getHistoricoFromAuno(id);
     }
 }
--- projeto/api/src/main/java/com/alunoonline/api/service/MatriculaAlunoService.java
+++ projeto/api/src/main/java/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -1,11 +1,17 @@
 package com.alunoonline.api.service;
 
 import com.alunoonline.api.model.MatriculaAluno;
+import com.alunoonline.api.model.dtos.DisciplinasAlunoDto;
+import com.alunoonline.api.model.dtos.HistoricoAlunoDto;
 import com.alunoonline.api.model.dtos.MatriculaAlunoNotasOnlyDto;
 import com.alunoonline.api.repository.MatriculaAlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -52,6 +58,65 @@ public class MatriculaAlunoService {
 
             repository.save(matriculaAlunoToUpdate.get());
         }
+    }
+
+    public void updateStatusToLocked(Long matriculaAlunoId){
+        Optional<MatriculaAluno> matriculaAlunoToUpdate = repository.findById(matriculaAlunoId);
+
+        if(matriculaAlunoToUpdate.isPresent()){
+            MatriculaAluno matriculaAluno = matriculaAlunoToUpdate.get();
+
+            String currentStatus = matriculaAluno.getStatus();
+
+            if(currentStatus.equals("MATRICULADO")){
+                matriculaAluno.setStatus("TRANCADO");
+            } else{
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Só é possível trancar uma matrícula com status MATRICULADO");
+            }
+
+            repository.save(matriculaAluno);
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Matrícula não encontrada");
+        }
+    }
+
+    public HistoricoAlunoDto getHistoricoFromAuno(Long alunoId){
+        List<MatriculaAluno> matriculasAluno = repository.findByAlunoId(alunoId);
+
+        if(!matriculasAluno.isEmpty()){
+            HistoricoAlunoDto historico = new HistoricoAlunoDto();
+
+            historico.setNomeAluno(matriculasAluno.get(0).getAluno().getNome());
+            historico.setCurso(matriculasAluno.get(0).getAluno().getCurso());
+
+            List<DisciplinasAlunoDto> disciplinasLista = new ArrayList<>();
+
+            for (MatriculaAluno matriculaAluno : matriculasAluno){
+                DisciplinasAlunoDto disciplinasAlunoDto = new DisciplinasAlunoDto();
+
+                disciplinasAlunoDto.setNomeDisciplina(matriculaAluno.getDisciplina().getNome());
+                disciplinasAlunoDto.setNomeProfessorDisciplina(matriculaAluno.
+                        getDisciplina().getProfessor().getNome());
+                disciplinasAlunoDto.setNota1(matriculaAluno.getNota1());
+                disciplinasAlunoDto.setNota2(matriculaAluno.getNota2());
+
+                if(matriculaAluno.getNota1() != null && matriculaAluno.getNota2() != null){
+                    disciplinasAlunoDto.setMedia((matriculaAluno.getNota1() + matriculaAluno.getNota2())/2);
+                } else {
+                    disciplinasAlunoDto.setMedia(null);
+                }
+
+                disciplinasAlunoDto.setStatus(matriculaAluno.getStatus());
+                disciplinasLista.add(disciplinasAlunoDto);
+            }
+
+            historico.setDisciplinasAlunoDtoList((disciplinasLista));
+
+            return historico;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Esse aluno não possui disciplinas matriculadas.");
     }
 
 }
